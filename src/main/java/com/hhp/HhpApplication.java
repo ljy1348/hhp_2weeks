8853ba2e338a264aaa1328e686d8202c6ed4a054
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-10-01 22:46:42 +0900

    chore :프로젝트 생성

---
+++ src/main/java/com/hhp/HhpApplication.java
@@ -0,0 +1,13 @@
+package com.hhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhpApplication.class, args);
+	}
+
+}
